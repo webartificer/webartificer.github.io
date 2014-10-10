2c8203a9de439cee41e4c46d7288c2c867caed11
webartificer <webartificer@gmail.com>
2014-10-10 10:37:02 -0700

    Update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(window).on('load resize', function(){
+	
+	var nav = $('nav');
+	var social_show = $('#social_block')
+	var wHeight = $(window).height() - 70;
+
+	
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > wHeight) {
+			nav.addClass("f_nav");
+			social_show.addClass("social_on");
+			social_show.addClass("wow");
+		} else {
+			nav.removeClass("f_nav");
+			social_show.removeClass("social_on");
+			social_show.removeClass("wow");
+		}
+	});
+
+});
---
+++ js/plugins.js
@@ -0,0 +1,45 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Single Page Navigation Plugin.
+
+// Prevent console.log from generating errors in IE for the purposes of the demo
+if ( ! window.console ) console = { log: function(){} };
+
+// The actual plugin
+$('nav').singlePageNav({
+    offset: $('nav').outerHeight(),
+    filter: ':not(.external)',
+    updateHash: true,
+    beforeStart: function() {
+        console.log('begin scrolling');
+    },
+    onComplete: function() {
+        console.log('done scrolling');
+    }
+});
+
+
+
+// Place any jQuery/helper plugins in here.
+ new WOW().init();
